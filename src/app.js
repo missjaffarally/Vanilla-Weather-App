206dcac3e8e1c2eda262d953fe216d53d16e7a32
missjaffarally <bibi.jaffarally@devopsgroup.com>
2022-01-28 14:52:53 +0000

    image update

--- src/app.js
+++ src/app.js
@@ -1,28 +1,50 @@
-function displayTemperature(response) {
+let apiKey = "4d65488fa2102701fc4a01966e06efc0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Guyana&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
+
+function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let iconElement = document.querySelector("#icon");
+
   cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.date.dt * 1000);
+  dateElement.innerHTML = dateElement.innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `$(day) $(hours) $(minutes)`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-
-let apiKey = "c2d8e3f5aacc591a6b55c63256699a8e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?g=London,uk&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
